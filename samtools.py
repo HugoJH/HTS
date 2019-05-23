6ad3ef4a6eb67bd77cd23ec329f2148a66c6d38e
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-05-23 09:57:48 +0200

    Added samtools file

---
+++ samtools.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#from collections import defaultdict
+#from subprocess import check_output
+#import os.path as path
+import multiprocessing
+#import os
+#import sys
+#import datetime
+#import hashlib
+#import inspect
+#from inspect import signature
+#import inspect
+#from pathlib import Path
+import utils
+
+def index_bam(bam,cores=multiprocessing.cpu_count()):
+    check_output(["samtools index -@{cores} {bam}".format(bam=bam,cores=cores) ], shell=True)
+
