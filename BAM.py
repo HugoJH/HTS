e5fffc07c4b105b46d76500011984ed829ff8a03
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-05-24 12:55:46 +0200

    Several fixes

--- BAM.py
+++ BAM.py
@@ -16,32 +16,30 @@ from GATK import *
 from bwa import bwaMem
 
 def UBAM_to_BAM(input_ubam, output, reference, interval_list, known_sites, tmp_dir):
-    file_id = md5(input_bam)
-    markAdapters(input_bam, f"{tmp_dir}/MarkedAdapters_{file_id}.bam", f"{tmp_dir}/MarkedAdapters_{file_id}.metrics.txt","{tmp_dir}")
+    file_id = md5(input_ubam)
+    markAdapters(input_ubam, f"{tmp_dir}/MarkedAdapters_{file_id}.bam", f"{tmp_dir}/MarkedAdapters_{file_id}.metrics.txt",tmp_dir)
 
-    samToFastq(f"{tmp_dir}/MarkedAdapters_{file_id}.bam", f"{tmp_dir}/SamToFastq_{file_id}.fq", "{tmp_dir}")
+    samToFastq(f"{tmp_dir}/MarkedAdapters_{file_id}.bam", f"{tmp_dir}/SamToFastq_{file_id}.fq", tmp_dir)
 
     bwaMem(f"{tmp_dir}/SamToFastq_{file_id}.fq", f"{tmp_dir}/bwaMem_{file_id}.bam", reference)
 
-    mergeBamAlignment(f"{tmp_dir}/MarkedAdapters_{file_id}.bam", f"{tmp_folder}/bwaMem_{file_id}.bam", f"{tmp_dir}/mergeBamAlignment_{file_id}.bam", reference, "{tmp_dir}")
+    mergeBamAlignment(f"{tmp_dir}/MarkedAdapters_{file_id}.bam", f"{tmp_dir}/bwaMem_{file_id}.bam", f"{tmp_dir}/mergeBamAlignment_{file_id}.bam", reference, tmp_dir)
 
-    markDuplicates(f"{tmp_dir}/mergeBamAlignment_{file_id}.bam", f"{tmp_folder}/markDuplicates_{file_id}.bam", f"{tmp_folder}/markduplicates_{file_id}.bam.txt", "{tmp_dir}")
+    markDuplicates(f"{tmp_dir}/mergeBamAlignment_{file_id}.bam", f"{tmp_dir}/markDuplicates_{file_id}.bam", f"{tmp_dir}/markduplicates_{file_id}.bam.txt", tmp_dir)
 
-    BaseRecalibrator(f"{tmp_dir}/markDuplicates_{file_id}.bam", f"{tmp_folder}/BaseRecalibrator_{file_id}.bam", reference, interval_list, known_sites)
+    BaseRecalibrator(f"{tmp_dir}/markDuplicates_{file_id}.bam", f"{tmp_dir}/BaseRecalibrator_{file_id}.bam", reference, interval_list, known_sites)
 
-    applyBQSR(f"{tmp_dir}/markDuplicates_{file_id}.bam", output, f"{tmp_folder}/BaseRecalibrator_{file_id}.bam", reference)
+    applyBQSR(f"{tmp_dir}/markDuplicates_{file_id}.bam", output, f"{tmp_dir}/BaseRecalibrator_{file_id}.bam", reference)
 
     try:
         validateSamFile(output, f"{tmp_dir}/ValidateSamFile_{file_id}.out", reference)
     except:
         print(f"{tmp_dir}/applyBQSR_{file_id}.bam didn't pass validation")
 
-    os.rename(f"{tmp_dir}/applyBQSR_{file_id}.bam", output)
-
 def BAM_to_UBAM(input_bam, output, tmp_dir):
     file_id = md5(input_bam)
     revertSam(input_bam, output, tmp_dir)
 
 def FASTQS_to_UBAM(fastq1, fastq2, output, readgroup_name, sample_name):
-    file_id = md5(input_bam)
+    file_id = md5(fastq1)
     fastqToSam(fastq1, fastq2, output, readgroup_name, sample_name)
