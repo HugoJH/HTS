99834db181f32a6e89cbdbd5a65d6a0d240241bf
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-06-04 09:20:34 +0200

    Minor fixes

--- VCF.py
+++ VCF.py
@@ -13,9 +13,9 @@ def BAM_to_VCF(tumor_bam, normal_bam, output, germline_resource, reference, inte
     workdir = path.dirname(output)
     file_id = getSampleName(tumor_bam,"/tmp/variant.id")
     if not path.isfile(tumor_bam + ".bai"):
-        index_bam(tumor_bam)
+        index_bam(tumor_bam, tumor_bam + ".bai")
     if not path.isfile(normal_bam + ".bai"):
-        index_bam(normal_bam)
+        index_bam(normal_bam, normal_bam + ".bai")
     mutect(tumor_bam, normal_bam, f"{workdir}/Mutect2_{file_id}.vcf", germline_resource, reference, interval_list, pon, ip)
     learnReadOrientationModel(f"{workdir}/Mutect2_{file_id}.vcf.f1r2.tar.gz",f"{workdir}/{file_id}-artifact-prior.tsv.tar.gz")
     GetPileupSummaries(tumor_bam, f"{workdir}/GetPileupSummaries_{file_id}.table" , small_exac_vcf, interval_list, ip="50")
--- samtools.py
+++ samtools.py
@@ -3,6 +3,6 @@ from subprocess import check_output
 import multiprocessing
 import utils
 
-def index_bam(bam,cores=multiprocessing.cpu_count()):
-    check_output(["samtools index -@{cores} {bam}".format(bam=bam,cores=cores) ], shell=True)
+def index_bam(bam, output, cores=multiprocessing.cpu_count()):
+    check_output(["samtools index -@{cores} {bam} {output}".format(bam=bam,output=output,cores=cores) ], shell=True)
 
